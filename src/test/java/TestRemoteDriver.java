dc6f4b1605294d4036ee9b016d775f0fc649a3cc
Swethapa1 <40977260+Swethapa1@users.noreply.github.com>
2018-10-29 14:55:49 +0530

    Add files via upload

--- src/test/java/TestRemoteDriver.java
+++ src/test/java/TestRemoteDriver.java
@@ -1,7 +1,10 @@
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -13,26 +16,28 @@ import utils.GetProperties;
 
 public class TestRemoteDriver {
 	Map<String, String> properties;
-	RemoteWebDriver driver;
+	WebDriver driver;
+	WebDriver driver1;
 	
 	@BeforeClass
-	private void setUp() {
+	private void setUp() throws MalformedURLException {
 		properties=new HashMap<String, String>();
 		properties = GetProperties.readProperties("login.properties");
 		launchBrowser();
 	}
 	
-	private void launchBrowser() {
+	private void launchBrowser() throws MalformedURLException {
 		DesiredCapabilities capabilities = new DesiredCapabilities();
-	    capabilities.setPlatform(Platform.LINUX);
+	   // capabilities.setPlatform(Platform.LINUX);
 	    capabilities.setBrowserName(properties.get("url"));
 	    ChromeOptions chromeOptions = new ChromeOptions();
-		System.setProperty("webdriver.chrome.driver", "./src/main/resources/linux/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "./src/main/resources/chromedriver");
 		chromeOptions.addArguments("--headless");
 		chromeOptions.addArguments("--disable-gpu");
 		chromeOptions.addArguments("--disable-extensions");
 		chromeOptions.addArguments("--no-sandbox");
-	    driver = new ChromeDriver(chromeOptions);
+	    //driver = new ChromeDriver(chromeOptions);
+		driver1 = new RemoteWebDriver(new URL(properties.get("url")),capabilities);
 	    driver.navigate().to(properties.get("url"));
 	}
 	
